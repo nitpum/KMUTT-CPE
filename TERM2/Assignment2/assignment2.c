cebd358f84348303768d2203616c70c08edc6e7f
nitpum <nitpum@outlook.com>
2018-02-05 23:05:31 +0700

    Update Assignment 2/18

--- TERM2/Assignment2/assignment2.c
+++ TERM2/Assignment2/assignment2.c
@@ -2,22 +2,15 @@
 #include <string.h>
 #include <ctype.h>
 
-
 void toLower(char *s) {
-    int i;
-    for (i = 0; s[i]; i++) s[i] = tolower(s[i]);
+    int i; for (i = 0; s[i]; i++) s[i] = tolower(s[i]);
 }
 
-int isnumber(char s[]) {
-    double num;
+int is_number(char s[]) {
     int i, count = 0;
-    for (i=0; i < strlen(s); i++) {
-        if (isdigit(s[i])==0) {
-            return 0;
-        } else  {
-            count++;
-        }
-    }
+    for (i=0; i < strlen(s); i++)
+        if (isdigit(s[i])==0) return 0;
+        else count++;
     return count;
 }
 
@@ -41,29 +34,19 @@ int getparameter (char input[], char cmd[], char p1[], char p2[], char p3[]) {
 int command (char name[], int paraCount, int minDigit, char cmd[], char p1[], char p2[], char p3[], int pCount, int * err) {
 
     if (strcmp(name,cmd) == 0) {
-        if (minDigit > 0 && paraCount <= pCount) {
-            if (paraCount >= 1 && isnumber(p1) == 0) *err = 1;
-            if (paraCount >= 2 && isnumber(p2) == 0) *err = 1;
-            if (paraCount >= 3 && isnumber(p3) == 0) *err = 1;
-
-            int digitCount = 0;
-            if (pCount >= 1) digitCount += isnumber(p1);
-            if (pCount >= 2) digitCount += isnumber(p2);
-            if (pCount >= 3) digitCount += isnumber(p3);
-            if (digitCount < minDigit) *err = 1;
-
+        if (paraCount <= pCount) {
+            if (paraCount >= 1 && is_number(p1) == 0) *err = 1;
+            if (paraCount >= 2 && is_number(p2) == 0) *err = 1;
+            if (paraCount >= 3 && is_number(p3) == 0) *err = 1;
             if (*err == 1) return 1;
 
-            *err = 0;
-            return 1;
-
-        } else
-        if (paraCount == pCount) {
-            if (paraCount >= 1 && isnumber(p1) == 0) *err = 1;
-            if (paraCount >= 2 && isnumber(p2) == 0) *err = 1;
-            if (paraCount >= 3 && isnumber(p3) == 0) *err = 1;
-
-            if (*err == 1) return 1;
+            if (minDigit > 0) {
+                int digitCount = 0;
+                if (pCount >= 1) digitCount += is_number(p1);
+                if (pCount >= 2) digitCount += is_number(p2);
+                if (pCount >= 3) digitCount += is_number(p3);
+                if (digitCount < minDigit) { *err = 1; return 1; }
+            } else if (paraCount < pCount) { *err = 1; return 1; }
 
             *err = 0;
             return 1;
@@ -71,23 +54,20 @@ int command (char name[], int paraCount, int minDigit, char cmd[], char p1[], ch
         *err = 1;
         return 1;
     }
-
     return 0;
 }
 
 int main () {
-
     char cmd[50];
     int count, err;
     do
     {
         rewind(stdin);
         char input[100];
+        char p1[50], p2[50], p3[50];
         printf("command> ");
         gets(input);
-        char p1[50], p2[50], p3[50];
-        count = 0;
-        count = getparameter(input, cmd, p1, p2, p3);
+        count = 0; count = getparameter(input, cmd, p1, p2, p3);
         err = -1;
         toLower(cmd);
         if (command("list", 0, 0, cmd, p1, p2, p3, count, &err) && err == 0) printf("answer> OK\n");
@@ -105,10 +85,8 @@ int main () {
         if (err == -1)      printf("answer> syntax error\n");
         else if (err >= 1) printf("answer> parameters error \n");
 
-
     } while (!(strcmp(cmd, "end") == 0 && count == 0));
 
     printf("End program\n");
     printf("Program writen by 60070501029 Nitipoom Unrrom");
-
 }
