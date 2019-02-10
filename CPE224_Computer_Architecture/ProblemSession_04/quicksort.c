9a831666fd9a85659e2de99b307aae105e4a29cc
nitpum <nitpum@outlook.com>
2019-02-10 23:19:22 +0700

    Clean code

--- CPE224_Computer_Architecture/ProblemSession_04/quicksort.c
+++ CPE224_Computer_Architecture/ProblemSession_04/quicksort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 
-int quicksort (int nums[], int low, int high, int round)
+int quicksort (int nums[], int low, int high)
 {
   int pivot = nums[low], i, j;
-  // Paritioning
+  // Paritioning Lomuto
   for (i =low + 1, j =low; i < high; i++)
   {
     if (nums[i] <= pivot)
@@ -13,28 +13,28 @@ int quicksort (int nums[], int low, int high, int round)
       int x = nums[j];
       nums[j] = nums[i];
       nums[i] = x;
-      //printf("Swap %d, %d : %d  : %d | %d\n", i, j,  nums[i], nums[j], pivot);
     }
   }
-  // Swap pivot
+  // Swap pivot value
   int x = nums[low];
   nums[low] = nums[j];
   nums[j]  = x;
-  printf("\n%d) ", round);
-  for (i = 0; i < 9; i++)
-  {
-    printf("%d, ", nums[i]);
-  }
+  // Recursive
   if (low < high)
   {
-      quicksort(nums, low, j - 1, round + 1);
-      quicksort(nums, j + 1, high, round + 2);
+      quicksort(nums, low, j - 1);
+      quicksort(nums, j + 1, high);
   }
 }
 
 int main ()
 {
   int nums[9] = {54, 26, 93, 17, 77, 31, 44, 55, 20}, i = 0;
-  quicksort(nums, 0, 9, 0);
+  quicksort(nums, 0, 9);
+  // Print result
+  for (i = 0; i < 9; i++)
+  {
+    printf("%d ", nums[i]);
+  }
   return 1;
 }
